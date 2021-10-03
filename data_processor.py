c1d9ac41fc5587eba5ae0db6371b3b54607e4517
Anirudh <ap1631@physics.rutgers.edu>
2021-10-02 20:53:19 -0400

    Update data_processor.py
    Changed function names to include more commonly used terminology.
    Made functions more efficient (fewer lines of code).
    Added a 'mixing' function.

--- data_processor.py
+++ data_processor.py
@@ -1,44 +1,37 @@
-# This function takes a standardized isotope ratio list as an input and converts it to mass fraction ration. The standards and scale (delta, epsilon, or mu) must also be inputted as a list and string respectively.
-def to_mfrac_ratio(ratio, std, scale):
-        mfrac_ratio = []
-        if scale == 'delta':
-            for i in range(len(ratio)):
-                mfrac_ratio.append((ratio[i]/1000 + 1) * std[i])
-        elif scale == 'epsilon':
-            for i in range(len(ratio)):
-                mfrac_ratio.append((ratio[i]/10000 + 1) * std[i])
-        elif scale == 'mu':
-            for i in range(len(ratio)):
-                mfrac_ratio.append((ratio[i]/1000000 + 1) * std[i])
-        return mfrac_ratio
+def to_iso_ratio(ratio, standard, scale = 'delta'):
+    # this function converts isotopic data in delta, epsiolon, or mu notation to isotopic ratios
+        iso_ratio = []
+        scales = {'delta':1000, 'epsilon': 10000, 'mu':1000000}
+        for i in range(len(ratio)):
+            iso_ratio.append((ratio[i]/scales[scale] + 1) * standard[i])
+        return iso_ratio
 
-# This function does the opposite of the above function. Taking mass fraction ratio list as input and converting to a standardized isotope ratio list scaled to either delta, epsilon, or mu.
-def to_scaled_ratio(mfrac, std, scale):
-        ratio = []
-        if scale == 'delta':
-            for i in range(len(mfrac)):
-                ratio.append((mfrac[i]/std[i]-1) * 1000)
-        elif scale == 'epsilon':
-            for i in range(len(mfrac)):
-                ratio.append((mfrac[i]/std[i]-1) * 10000)
-        elif scale == 'mu':
-            for i in range(len(mfrac)):
-                ratio.append((mfrac[i]/std[i]-1) * 1000000)
-        return ratio
+def to_scaled_ratio(iso_ratio, standard, scale = 'delta'):
+    # this function converts isotopic ratios into delta, epsilon, or my notation
+        scaled_ratio = []
+        scales = {'delta':1000, 'epsilon': 10000, 'mu':1000000}
+        for i in range(len(iso_ratio)):
+            scaled_ratio.append((iso_ratio[i]/standard[i]-1) * scales[scale])
+        return scaled_ratio
 
-# This function takes a mass fraction ratio list and gets the absolute mass fractions (within the sample) for each isotope.
-def get_mfrac(mfrac_ratio):
+def get_mfrac(iso_ratio):
+    # this function gets mass fractions from isotopic ratios
         mfrac = []
-        tot = sum(mfrac_ratio)
-        for i in range(len(mfrac_ratio)):
-            mfrac.append(mfrac_ratio[i]/tot)
+        tot = sum(iso_ratio)
+        for i in range(len(iso_ratio)):
+            mfrac.append(iso_ratio[i]/tot)
         return mfrac
 
-# This function takes an absolute mass fraction list and reference isotope (the desired denominator in the mass fraction ratios) and outputs a mass fraction ratio list.
-def get_mfrac_ratio(mfrac, ref_iso):
+def get_iso_ratio(mfrac, normalization_isotope):
+    # this function gets isotopic ratios from mass fractions and a specified normalization mass fraction
         mfrac_ratio = []
         for i in range(len(mfrac)):
-            mfrac_ratio.append(mfrac[i]/ref_iso)
+            mfrac_ratio.append(mfrac[i]/normalization_isotope)
         return mfrac_ratio
 
-
+def mix_mfrac(N1, N2, F):
+    # This function performs a mixing calculation between two materials, N1 and N2, as specified by the mixing factor F.
+        N_calc = []
+        for i in range(len(N1)):
+            N_calc.append(N1[i] + F * (N2[i] - N1[i]))
+        return N_calc
